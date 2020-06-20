619ebebced101e6c33e15ee9bb8f87eec229611d
Eknoor Oberoi <eknoor.oberoi@gmail.com>
2020-06-20 18:48:09 +0530

    controlling userEffect behaviour

--- src/App.js
+++ src/App.js
@@ -108,24 +108,31 @@ const App =() => {
   //by default news array is []
   const [news, setNews] = useState([]);
   const [searchQuery, setSearchQuery] = useState('react');
+  const [url, setUrl] = useState('http://hn.algolia.com/api/v1/search?query=react');
   //fetch news from hack and news Api
   const fetchNews = () => {
-    fetch(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+    fetch(url)
     .then(result => result.json())
     .then(data => setNews(data.hits))
     .catch(error => console.log(error));
   };
   // to get news we need to execute this method, it will not run by itself
   //we want it to work only when there is change in searchQuery
-  useEffect(() => {fetchNews();}, [searchQuery]);
-  
+  useEffect(() => {fetchNews();}, [url]);
+
   const handleChange=(e) => {
     setSearchQuery(e.target.value);
   };
+
+  const handleSubmit =e => {
+    e.preventDefault()
+    //set url based on user input
+    setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+  }
   return (
     <div>
       <h2>News</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="text" value={searchQuery} onChange={handleChange}/>
         <button>Search</button>
       </form>
