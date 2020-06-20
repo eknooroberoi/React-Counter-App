13c45f4ba079af50d4a405c2436500c9d2a4a615
Eknoor Oberoi <eknoor.oberoi@gmail.com>
2020-06-20 18:09:33 +0530

    search news based on input change

--- src/App.js
+++ src/App.js
@@ -106,21 +106,29 @@ import React, {Component, useState, useEffect} from 'react';
 const App =() => {
   //state
   //by default news array is []
-  const [news, setNews] = useState([])
+  const [news, setNews] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('react');
   //fetch news from hack and news Api
   const fetchNews = () => {
-    fetch('http://hn.algolia.com/api/v1/search?query=react')
+    fetch(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
     .then(result => result.json())
     .then(data => setNews(data.hits))
     .catch(error => console.log(error));
   };
   // to get news we need to execute this method, it will not run by itself
-  useEffect(() => {
-    fetchNews()
-  })
+  //we want it to work only when there is change in searchQuery
+  useEffect(() => {fetchNews();}, [searchQuery]);
+  
+  const handleChange=(e) => {
+    setSearchQuery(e.target.value);
+  };
   return (
     <div>
       <h2>News</h2>
+      <form>
+        <input type="text" value={searchQuery} onChange={handleChange}/>
+        <button>Search</button>
+      </form>
       {news.map((n, i) => (
         <p key={i}>{n.title}</p>
         ))}
